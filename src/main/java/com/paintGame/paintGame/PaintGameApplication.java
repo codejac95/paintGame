311c468a1533549cf532af61a1070bcec5ecf53b
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-09-02 09:56:34 +0200

    dep

---
+++ src/main/java/com/paintGame/paintGame/PaintGameApplication.java
@@ -0,0 +1,13 @@
+package com.paintGame.paintGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paintGame/paintGame/PaintGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paintGame.paintGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
