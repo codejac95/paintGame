985880c707bef25fd6eed81de6ef0f30ffed5cd3
Georg <kalla.georg@yahoo.com>
2024-09-08 10:12:46 +0200

    image presentation with countdown

--- src/main/java/com/paintGame/paintGame/models/ImageMessage.java
+++ src/main/java/com/paintGame/paintGame/models/ImageMessage.java
@@ -2,13 +2,14 @@ package com.paintGame.paintGame.models;
 
 public class ImageMessage {
     private String image;
+    private String action;
 
-    public ImageMessage(){
-
+    public ImageMessage() {
     }
 
-    public ImageMessage(String image) {
+    public ImageMessage(String image, String action) {
         this.image = image;
+        this.action = action;
     }
 
     public String getImage() {
@@ -18,6 +19,14 @@ public class ImageMessage {
     public void setImage(String image) {
         this.image = image;
     }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
     
-    
+
 }
