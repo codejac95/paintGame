2cfd1d8372c6f7862f40458279be258f5e9fa07e
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-09-10 15:23:56 +0200

    tillåter alla att komma åt

--- src/main/java/com/paintGame/paintGame/config/WebSocketConfig.java
+++ src/main/java/com/paintGame/paintGame/config/WebSocketConfig.java
@@ -20,8 +20,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://seashell-app-ia2eg.ondigitalocean.app/")
-                .withSockJS();
+        .setAllowedOriginPatterns("*").withSockJS(); 
+           
     }
   
 }
